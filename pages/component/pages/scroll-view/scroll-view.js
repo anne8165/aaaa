1ecd1e0a81da33699f96f3d1e65780fea2f9fa8c
anne123 <1437485788@qq.com>
2018-05-04 15:31:49 +0800

    scroll-view完善

--- pages/component/pages/scroll-view/scroll-view.js
+++ pages/component/pages/scroll-view/scroll-view.js
@@ -1,1 +1,31 @@
-Page({})
+var order = ['red', 'yellow', 'blue', 'green', 'red']
+Page({
+  data: {
+    toView: 'yellow',
+    scrollTop: 0
+  },
+  scrolltoupper : function (e) {
+    console.log(e)
+  },
+  lower: function (e) {
+    console.log(e)
+  },
+  scroll: function (e) {
+    console.log(e)
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 100
+    })
+  }
+})
